931746bf2ed420f596b2fca6f3c6d458c2420691
Zenon <166578664+ZenonHh@users.noreply.github.com>
2025-04-25 15:17:07 +0700

    Add files via upload

--- history.php
+++ history.php
@@ -24,156 +24,48 @@ $sql = "SELECT orders.*, users.fullname, users.numberphone
 $result = mysqli_query($conn, $sql);
 $stt = 1;
 ?>
-<head>
-    <meta charset="utf-8">
-    <title>MMB- Shop Bán Đồ Cầu Lông</title>
-    <link href='img/logo.png' rel='icon' type='image/x-icon' />
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
-
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
-
-    <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
-</head>
-
-<body>
-    <!-- Topbar Start -->
-   
-    <div class="container-fluid">
-       
-
-        <div class="row bg-secondary py-2 px-xl-5">
-            <div class="col-lg-6 d-none d-lg-block">
-                <div class="d-inline-flex align-items-center">
-                    <a class="text-dark" href="">Câu Hỏi Thường Gặp</a>
-                    <span class="text-muted px-2">|</span>
-                    <a class="text-dark" href="">Trợ Giúp</a>
-                    <span class="text-muted px-2">|</span>
-                    <a class="text-dark" href="">Hỗ Trợ</a>
-                </div>
-            </div>
-            <div class="col-lg-6 text-center text-lg-right">
-                <div class="d-inline-flex align-items-center">
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-linkedin-in"></i>
-                    </a>
-                    <a class="text-dark px-2" href="">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a class="text-dark pl-2" href="">
-                        <i class="fab fa-youtube"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div class="row align-items-center py-3 px-xl-5">
-            <div class="col-lg-3 d-none d-lg-block">
-                <a href="logedin.php" class="text-decoration-none">
-                    <div style="display: flex; align-items: center; position: relative;">
-                        <img src="img/logo.png" alt="a logo" width="85px" height="85px">
-                        <span class="custom-font" style="margin-left: 10px; position: relative; top: 20px;">Shop</span>
-                    </div>
+<?php include 'header.php'; ?>
+            <div class="container-fluid bg-white mb-2"> <!-- giảm khoảng cách -->
+    <div class="row border-top px-xl-5">
+        <div class="col-lg-12">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 px-0">
+                <a href="" class="text-decoration-none d-block d-lg-none">
+                    <h1 class="m-0 display-5 font-weight-semi-bold">
+                        <span class="text-primary font-weight-bold border px-3 mr-1">VNB</span>Shop
+                    </h1>
                 </a>
-            </div>
-            <div class="col-lg-6 col-6 text-left">
-                <form action="shoptimkiem_login.php">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Nhập nội dung bạn muốn tìm kiếm">
-                        <div class="input-group-append">
-                            <button class="input-group-text bg-transparent text-primary" class="fa fa-search">
-                                <i class="fa fa-search"></i>
-                            </button>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse d-flex justify-content-between w-100" id="navbarCollapse">
+                    <!-- Menu bên trái -->
+                    <div class="navbar-nav py-0">
+                        <a href="logedin.php" class="nav-item nav-link active">Trang Chủ</a>
+                        <a href="shoplogin.php" class="nav-item nav-link">Sản Phẩm</a>
+                        <a href="contact.php" class="nav-item nav-link">Liên Hệ</a>
+                    </div>
+
+                    <!-- Tài khoản bên phải nhưng đẩy vào trái 20px -->
+                    <div class="navbar-nav py-0" style="margin-right: 65px;"> <!-- thêm khoảng cách vào trái -->
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link" data-toggle="dropdown">
+                                <?php 
+                                echo isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : "Khách"; 
+                                ?>
+                            </a>
+                            <div class="dropdown-menu rounded-0 m-0">
+                                <a href="logout.php" class="dropdown-item">Đăng Xuất</a>
+                                <a href="suathongtinuser.php" class="dropdown-item">Đổi Thông Tin</a>
+                                <a href="history.php" class="dropdown-item">Lịch sử mua hàng</a>
+                            </div>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div class="col-lg-3 col-6 text-right">
-                <a href="" class="btn border">
-                    <i class="fas fa-heart text-primary"></i>
-                    <span class="badge">0</span>
-                </a>
-                <a href="cart.html" class="btn border">
-                    <i class="fas fa-shopping-cart text-primary"></i>
-                    <span class="badge">0</span>
-                </a>
-            </div>
+                </div>
+            </nav>
         </div>
     </div>
-    <!-- Topbar End -->
-
-
-    <!-- Navbar Start -->
-    <div class="container-fluid">
-        <div class="row border-top px-xl-5">
-            <div class="col-lg-3 d-none d-lg-block">
-                <a class="btn shadow-none d-flex align-items-center justify-content-between bg-primary text-white w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; margin-top: -1px; padding: 0 30px;">
-                    <h6 class="m-0">Phân Loại Sản Phẩm</h6>
-                    <i class="fa fa-angle-down text-dark"></i>
-                </a>
-                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 1;">
-                    <div class="navbar-nav w-100 overflow-hidden" style="height: 245px">
-                    <a href="vot_login.html" class="nav-item nav-link">Yonex</a>
-                        <a href="giay_login.html" class="nav-item nav-link">Lining</a>
-                        <a href="tui_login.html" class="nav-item nav-link">Victor</a>
-                        <a href="quan_login.html" class="nav-item nav-link">Mizuno</a>
-                        <a href="ao_login.html" class="nav-item nav-link">VNB</a>
-                        <a href="vay_login.html" class="nav-item nav-link">Apacs</a>
-                        
-                            
-                </nav>
-            </div>
-            <div class="col-lg-9">
-                <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
-                    <a href="logedin.php" class="text-decoration-none d-block d-lg-none">
-                        <div style="display: flex; align-items: center; position: relative;">
-                            <img src="img/logo.png" alt="a logo" width="85px" height="85px">
-                            <span class="custom-font" style="margin-left: 10px; position: relative; top: 20px;">Shop</span>
-                        </div> 
-                    </a>
-                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                        <div class="navbar-nav mr-auto py-0">
-                            <a href="logedin.php" class="nav-item nav-link">Trang chủ</a>
-                            <a href="shoplogin.php" class="nav-item nav-link">Sản Phẩm</a>
-                            <a href="contactlogin.php" class="nav-item nav-link">Liên Hệ</a>
-                        </div>
-                        <div class="navbar-nav ml-auto py-0">
-                             <div class="nav-item dropdown">
-                                <a href="#" class="nav-link" data-toggle="dropdown"><?php 
-                    echo isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : "Khách"; 
-                ?></a>
-                                <div class="dropdown-menu rounded-0 m-0">
-                                    <a href="index.php" class="dropdown-item">Đăng Xuất</a>
-                                    <a href="suathongtinuser.php" class="dropdown-item">Đổi Thông Tin</a>
-                                    <a href="history.php" class="dropdown-item">Lịch Sử Mua Hàng</a>
-                        </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </div>
-    
+</div>
     <!-- Navbar End -->
 
 
